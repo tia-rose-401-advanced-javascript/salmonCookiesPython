8e08c9c5eff2650f7b393ab5a50e595c8cc726a4
Tia Rose <shortyshort789@aol.com>
2019-07-13 11:37:45 -0700

    have styles connected, trying to figure out how to have multiple renders happen. But will research later, time for heroku

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # lowercase flask is the module I installed, uppercase Flask is the function will be executed in order to create the app object
 
-from flask import Flask
+from flask import Flask, render_template
 
 # Importing variables from settings file
 from settings import NAME
@@ -10,9 +10,14 @@ app=Flask(__name__)
 
 @app.route("/")
 def hello():
-  return f"Hello {NAME}"
+  # return f"Hello {NAME}"
+  f"NAME: {NAME}"
+  f"SECOND_NAME: {SECOND_NAME}"
+  return render_template("hello.html", name=NAME)
+  return render_template("hello.html", second_name=SECOND_NAME)
 
 
 @app.route("/test")
 def dustin():
-  return f"Hello {SECOND_NAME}"
+  f"SECOND_NAME: {SECOND_NAME}"
+  return render_template("hello.html", second_name=SECOND_NAME)
